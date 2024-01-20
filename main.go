92841c1b06762a0246ccf2bbd0e52f21edf5fcf0
SilentGrig <SilentGrig@gmail.com>
2024-01-20 18:18:30 +0000

    Add health endpoint and serve files from /app/ path

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerHealth)
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
@@ -20,6 +21,12 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
 func middlewareCors(next http.Handler) http.Handler {
